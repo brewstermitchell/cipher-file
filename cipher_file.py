46ff936abfe88a702e3c0b211fbfdf8c56657f17
brewstermitchell <brewster.mitchell@gmail.com>
2016-04-12 00:08:29 -0700

    draft complete

--- cipher_file.py
+++ cipher_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 ####
-# Cypher-file: contains functions to encypher and decypher files into base64. (weak cipher)
+# Cypher-file: contains functions to encipher and decipher files into base64. (weak cipher)
 # written by Brewster Mitchell
 #   April 11, 2016
 # See LICENSE.md for license terms.
@@ -13,20 +13,19 @@ import base64
 in_f = sys.argv[1]
 out_f = sys.argv[2]
 
-print(type(in_f))
-print(type(out_f))
 
 def encrypt (in_file, out_file):
-  with open(in_file) as f:
+  with open(in_file, 'rb') as f:
     encoded = base64.b64encode(f.read())
-    print(encoded)
-    with open(out_file, 'w') as w:
+    with open(out_file, 'wb') as w:
       w.write(encoded)
       
 def decrypt (out_file, in_file):
-  with open(out_file) as f:
+  with open(out_file, 'rb') as f:
     decoded = base64.b64decode(f.read())
-    print(decoded)
+    with open(in_file, 'wb') as w:
+      w.write(decoded)
+      
     
-# encrypt(in_f, out_f)
-# decrypt(out_f, in_f)
+encrypt(in_f, out_f)
+decrypt(out_f, 'result.xml')
