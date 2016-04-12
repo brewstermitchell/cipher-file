b13b97584f426f4fdfd587811f1c0d9a3ac04156
brewstermitchell <brewster.mitchell@gmail.com>
2016-04-12 00:20:11 -0700

    update

--- cipher_file.py
+++ cipher_file.py
@@ -14,13 +14,13 @@ in_f = sys.argv[1]
 out_f = sys.argv[2]
 
 
-def encrypt (in_file, out_file):
+def encrypt(in_file, out_file):
   with open(in_file, 'rb') as f:
     encoded = base64.b64encode(f.read())
     with open(out_file, 'wb') as w:
       w.write(encoded)
       
-def decrypt (in_file, out_file):
+def decrypt(in_file, out_file):
   with open(in_file, 'rb') as f:
     decoded = base64.b64decode(f.read())
     with open(out_file, 'wb') as w:
